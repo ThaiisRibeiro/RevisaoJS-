cdb0d094e6ada71d5d79fd0153548d4558f9ac51
ThaiisRibeiro <thais.asfur@gmail.com>
2024-08-14 16:32:04 -0300

    Revisao ;p

---
+++ Ex01.js
@@ -0,0 +1,15 @@
+function encontrarMaiorNumero(numeros) {
+    // Inicializa a variável maiorNumero com o primeiro elemento do array
+    let maiorNumero = numeros[0];
+
+    // Percorre o array começando do segundo elemento (índice 1)
+    for (let i = 1; i < numeros.length; i++) {
+        // Se o elemento atual for maior do que maiorNumero, atualiza maiorNumero
+        if (numeros[i] > maiorNumero) {
+            maiorNumero = numeros[i];
+        }
+    }
+
+    // Retorna o valor de maiorNumero
+    return maiorNumero;
+}
---
+++ ExExtra.js
@@ -0,0 +1,15 @@
+function encontrarMenorNumero(numeros) {
+    // Inicializa a variável menorNumero com o primeiro elemento do array
+    let menorNumero = numeros[0];
+
+    // Percorre o array começando do segundo elemento (índice 1)
+    for (let i = 1; i < numeros.length; i++) {
+        // Se o elemento atual for menor do que menorNumero, atualiza menorNumero
+        if (numeros[i] < menorNumero) {
+            menorNumero = numeros[i];
+        }
+    }
+
+    // Retorna o valor de menorNumero
+    return menorNumero;
+}
